7858248090181a8584a135f4b6251d4291a20e63
lucky0559 <angelorabosa555@gmail.com>
2023-08-09 12:59:03 +0800

    fixed count of order item

--- src/mockData/categories.ts
+++ src/mockData/categories.ts
@@ -6,60 +6,60 @@ export const categories: Category[] = [
     name: "Breakfast",
     items: [
       {
-        id: 1,
+        id: 2,
         name: "Eggsilog",
         price: 80
       },
       {
-        id: 2,
+        id: 3,
         name: "Bacsilog",
         price: 148
       }
     ]
   },
   {
-    id: 2,
+    id: 4,
     name: "Drinks",
     items: [
       {
-        id: 1,
+        id: 5,
         name: "Iced coffee",
         price: 176
       },
       {
-        id: 2,
+        id: 6,
         name: "Java Chip Cream",
         price: 210
       }
     ]
   },
   {
-    id: 3,
+    id: 7,
     name: "Soup",
     items: [
       {
-        id: 1,
+        id: 8,
         name: "Mushroom Soup",
         price: 100
       },
       {
-        id: 2,
+        id: 9,
         name: "Chicken Soup",
         price: 120
       }
     ]
   },
   {
-    id: 4,
+    id: 10,
     name: "Alcohol",
     items: [
       {
-        id: 1,
+        id: 11,
         name: "Soju",
         price: 210
       },
       {
-        id: 2,
+        id: 12,
         name: "Redhorse",
         price: 220
       }
